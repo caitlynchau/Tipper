7eaf82302beb9471955057840d9228d8fb57c389
Albert Chau <albertchau@albertteki-macbook-pro.local>
2018-12-24 11:58:34 -0800

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Albert Chau on 12/24/18.
+//  Copyright © 2018 Caitlyn Chau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
